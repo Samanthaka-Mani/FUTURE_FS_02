baf46a20e3efc4f1c6e7f0a2719cfa1f8fbb1440
Samanthaka-Mani <maniists4@gmail.com>
2025-08-03 10:28:02 +0530

    Use dynamic port from Render env

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   preview: {
-    port: 10000,
     host: true,
+    port: parseInt(process.env.PORT) || 4173, // use Render's port or default to 4173
     allowedHosts: ['mini-ecommerce-store-alhc.onrender.com']
   }
 })
