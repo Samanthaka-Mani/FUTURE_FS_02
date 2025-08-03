5191b6ed7e49d7d3349d16d85849548f13960fa4
Samanthaka-Mani <maniists4@gmail.com>
2025-08-03 11:32:22 +0530

    Update server.js and package.json after bug fix

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  preview: {
-    host: true,
-    port: parseInt(process.env.PORT) || 4173, // use Render's port or default to 4173
-    allowedHosts: ['mini-ecommerce-store-alhc.onrender.com']
-  }
-})
+});
